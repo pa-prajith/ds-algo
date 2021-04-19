e7ed27a198b8044163781ae3b42d4698cb133dc9
pa-prajith <codejs100@gmail.com>
2021-04-19 11:31:36 -0700

    Sum Range - Recursion

---
+++ Recursion/number_sumrange.js
@@ -0,0 +1,18 @@
+/*
+    Problem: Recurssive method to return sum range
+*/
+
+function sumRange(num) {
+  if (num <= 0) {
+    return 0;
+  }
+  if (num === 1) {
+    return 1;
+  }
+
+  return num + sumRange(num - 1);
+}
+
+console.log(`Sum Range 10 ${sumRange(10)}`);
+console.log(`Sum Range 5 ${sumRange(5)}`);
+console.log(`Sum Range -10 ${sumRange(-10)}`);

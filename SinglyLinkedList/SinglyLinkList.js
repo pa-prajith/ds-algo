b40fb12ac7271f7a223de294b56e8be22d3e4779
pa-prajith <codejs100@gmail.com>
2021-05-05 13:20:05 -0700

    Singly Link list pop

--- SinglyLinkedList/SinglyLinkList.js
+++ SinglyLinkedList/SinglyLinkList.js
@@ -23,6 +23,37 @@ class SinglyLinklist {
     this.length = this.length + 1;
     return this;
   }
+
+  pop() {
+    if (!this.head) {
+      return this;
+    }
+    if (this.length === 1) {
+      this.length = 0;
+      this.head = null;
+      this.tail = null;
+      return this;
+    }
+    let current = this.head;
+    let prevNode = null;
+    while (current.nextNode) {
+      prevNode = current;
+      current = current.nextNode;
+    }
+    this.tail = prevNode;
+    this.length = this.length - 1;
+    return this;
+  }
+
+  display() {
+    let current = this.head;
+    const arr = [];
+    while (current) {
+      arr.push(current.value);
+      current = current.nextNode;
+    }
+    console.log(arr.join(" --> "));
+  }
 }
 
 const list = new SinglyLinklist();
@@ -31,4 +62,8 @@ list.push("how");
 list.push("you");
 list.push("doing");
 
-console.log(list);
+list.display();
+list.pop();
+list.display();
+list.push("new One");
+list.display();

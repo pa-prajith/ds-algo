22d974118d2c733bb11030d49006988e241eff0e
pa-prajith <codejs100@gmail.com>
2021-05-05 17:04:02 -0700

    Linkedlist methods

--- SinglyLinkedList/SinglyLinkList.js
+++ SinglyLinkedList/SinglyLinkList.js
@@ -45,6 +45,106 @@ class SinglyLinklist {
     return this;
   }
 
+  unshift(value) {
+    const node = new LinkNode(value);
+    if (!this.head) {
+      this.tail = node;
+      this.head = node;
+    } else {
+      node.nextNode = this.head;
+      this.head = node;
+    }
+    this.length = this.length + 1;
+    return this;
+  }
+
+  shift() {
+    if (!this.head) {
+      return undefined;
+    }
+    const oldHead = this.head;
+    this.head = oldHead.nextNode;
+    this.length = this.length - 1;
+    if (this.length === 0) {
+      this.tail = null;
+    }
+    return oldHead;
+  }
+
+  get(index) {
+    if (index < 1 || index > this.length) {
+      return undefined;
+    }
+    let current = this.head;
+    let tracker = 1;
+    while (index !== tracker) {
+      current = current.nextNode;
+      tracker++;
+    }
+    return current;
+  }
+
+  set(index, value) {
+    /*
+    if (index < 1 || index > this.length) {
+      return undefined;
+    }
+    let current = this.head;
+    let tracker = 1;
+    while (current) {
+      if (index === tracker) {
+        current.value = value;
+        break;
+      }
+      current = current.nextNode;
+      tracker++;
+    }
+    return this;
+    */
+    let eleNode = this.get(index);
+    if (eleNode) {
+      eleNode.value = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    if (index === 0 && index > this.length) {
+      return false;
+    }
+    if (index === 1) {
+      this.unshift(value);
+    } else if (index === this.length) {
+      this.push(value);
+    } else {
+      const getIndex = index - 1;
+      const eleNode = this.get(getIndex);
+      const newNode = new LinkNode(value);
+      newNode.nextNode = eleNode.nextNode;
+      eleNode.nextNode = newNode;
+    }
+    this.length = this.length + 1;
+    return true;
+  }
+  remove(index) {
+    if (index === 0 && index > this.length) {
+      return false;
+    }
+    if (index === 1) {
+      this.shift();
+    } else if (index === this.length) {
+      this.pop();
+    } else {
+      const getIndex = index - 1;
+      const prevNode = this.get(getIndex);
+      const currentNode = prevNode.nextNode;
+      prevNode.nextNode = currentNode.nextNode;
+    }
+    this.length = this.length - 1;
+    return true;
+  }
+
   display() {
     let current = this.head;
     const arr = [];
@@ -66,4 +166,15 @@ list.display();
 list.pop();
 list.display();
 list.push("new One");
+list.shift();
+list.unshift("Hi... ");
+list.display();
+console.log(list.get(2));
+console.log(list.get(1));
+console.log(list.get(20));
+list.set(2, "Yooo!");
+list.insert(3, "New Insertion");
+list.insert(1, "heyyy");
+list.display();
+list.remove(2);
 list.display();
